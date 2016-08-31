b464094384634018f50107966bee117366437e1c
ovrmrw <t.ngc.0403@gmail.com>
2016-08-31 14:24:07 +0900

    Update

--- create-directory.ts
+++ create-directory.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 
 
-export function createCsvStoreDirectory(dirPath: string) {
+export function createCsvStoreDirectory(dirPath: string): void {
   if (!fs.existsSync(dirPath)) {
     fs.mkdirSync(dirPath);
   }
--- test.js
+++ test.js
@@ -15,3 +15,17 @@ console.log(moment().format("HHmm"));
 
 console.log("0850" < "1000");
 console.log("1450" < "1530");
+
+console.log('-'.repeat((80)));
+
+// console.log(moment().valueOf());
+// moment().locale('ja');
+// console.log(moment().locale('ja').valueOf());
+
+console.log(new Date().valueOf())
+console.log(moment().valueOf())
+console.log(new Date(new Date().valueOf()))
+
+console.log(moment(new Date().valueOf()).format()) // 特に指定はしていないが自動的に日本時間に変換されている。
+console.log(moment(new Date().valueOf()).format('YYYY-MM-DD hh:mm:ss a'))
+console.log(moment(moment().valueOf()).format('YYYY-MM-DD hh:mm:ss a'))
